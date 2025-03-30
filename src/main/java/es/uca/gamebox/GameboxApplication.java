c602b6a554cdf565c49a285b099281699a104e4b
Lynel <manuoteber@gmail.com>
2025-03-30 13:33:19 +0200

    Initial commit

---
+++ src/main/java/es/uca/gamebox/GameboxApplication.java
@@ -0,0 +1,13 @@
+package es.uca.gamebox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameboxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GameboxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/es/uca/gamebox/GameboxApplicationTests.java
@@ -0,0 +1,13 @@
+package es.uca.gamebox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameboxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
